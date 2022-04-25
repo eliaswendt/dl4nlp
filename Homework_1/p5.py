0e02ade05fc5fce054ec29586bf078a0b0cd6da7
Elias Wendt <git@eliaswendt.de>
2022-04-25 14:15:53 +0200

    Fix problems in read_data(), implement loss(), implement backward() and main training loop
    Co-authored-by: boyettelk <boyettelk@users.noreply.github.com>

--- Homework_1/p5.py
+++ Homework_1/p5.py
@@ -1,3 +1,4 @@
+from re import U
 import numpy as np
 import os
 
@@ -12,10 +13,13 @@ def read_dataset(filepath: str):
         for line in f:
             _, sentiment_str, embedding_str = line.split("\t")
 
-            sentiment = True if sentiment_str == "label=POS" else False
+            y = 1.0 if sentiment_str == "label=POS" else 0.0
 
-            # transform embeddings to np.array
-            embedding = np.array()
+            embedding = [float(x) for x in embedding_str.split(" ")]
+            embedding.append(1.0)
+            x = np.array(embedding)
+
+            datapoints.append((x, y))
 
     return datapoints
 
@@ -26,33 +30,87 @@ def read_dataset(filepath: str):
 def sig(x):
     return 1 / (1 + np.exp(-x))
 
+def sig_deriv(x):
+    return sig(x)*(1-sig(x))
+
 # ----------------------------------------------------------------------------------------------------------------------
 #               5.3 TRAINING
 # ----------------------------------------------------------------------------------------------------------------------
 
 def forward(x, w):
-    return sig(x.transpose * w)
+    print(f"x.shape={x.shape}, w.shape={w.shape}")
+    return sig(x.T.dot(w)) 
 
 def backward(w, lr, batch):
-    w = 
 
-datapoints = read_dataset("DATA/rt-polarity.dev.vecs")
-# organize input in batches
-# shuffle batches
+    sum = np.zeros(len(batch))
 
-w = np.random.normal(0, 1, (N, 1))
+    for x, y in batch:
 
+        part1 = (sig(x.T.dot(w)) - y)
+        part2 = sig_deriv(x.T.dot(w))
 
+        print(f"part1.shape={part1.shape}, part2.shape={part2.shape}, x.shape={x.shape}")
 
-for epoch in (n_epochs):
-    for X, Y in batches:
+        sum += part1 * part2 * x
 
-        # forward
-        y = forward(x, w)
-        
-        # calculate loss
-        loss = 
+    print(f"sum.shape={sum.shape}, w.shape={w.shape}")
 
-        # backward (update weights)
+    w -= (lr / len(batch)) * sum
 
 
+
+def loss(y, y_hat):
+    return (y_hat - y)**2 
+
+def train():
+    # initialize weights randomly (with seed)
+    np.random.seed(100)
+
+
+    datapoints_dev = read_dataset("DATA/rt-polarity.dev.vecs")
+    datapoints_test = read_dataset("DATA/rt-polarity.test.vecs")
+    datapoints_train = read_dataset("DATA/rt-polarity.train.vecs")
+
+    datapoints = datapoints_dev
+
+    # organize input in batches
+    # shuffle batches
+
+    N = len(datapoints[0][0]) # number of lines/datapoints
+    w = np.random.normal(0, 1, (N, 1)) 
+
+    lr = .01
+    n_epochs = 50
+    batch_size = 10
+
+    n_batches = np.ceil(len(datapoints) / batch_size)
+    print(f"n_batches={n_batches}")
+
+    for epoch in range(n_epochs):
+
+        # shuffle batches before each epoch
+        np.random.shuffle(datapoints)
+
+        # form batches
+        batches = np.array_split(datapoints, n_batches)
+
+        for n_batch, batch in enumerate(batches):
+
+            loss_sum = 0
+
+            # iterate over all datapoints in this batch
+            for x, y in batch:
+                # forward
+                y_hat = forward(x, w)
+                print(f"y_hat.shape={y_hat.shape}")
+
+                loss_sum += loss(y, y_hat)
+
+            print(f"epoch={epoch}, n_batch={n_batch}, loss_sum={loss_sum}")
+
+            # backward
+            backward(w, lr, batch)
+
+if __name__ == "__main__":
+    train()
