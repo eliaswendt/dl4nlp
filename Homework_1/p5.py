Fix shape problem and add metrics calculation
Co-authored-by: boyettelk <boyettelk@users.noreply.github.com>

Homework_1/p5.py
@@ -1,6 +1,7 @@
 from re import U
 import numpy as np
 import os
+#from tqdm import tqdm
 
 # ----------------------------------------------------------------------------------------------------------------------
 #               5.1 DATASET READER
@@ -13,11 +14,11 @@ def read_dataset(filepath: str):
         for line in f:
             _, sentiment_str, embedding_str = line.split("\t")
 
-            y = 1.0 if sentiment_str == "label=POS" else 0.0
+            y = np.array(1.0 if sentiment_str == "label=POS" else 0.0)
 
-            embedding = [float(x) for x in embedding_str.split(" ")]
+            embedding = [x for x in embedding_str.split(" ")]
             embedding.append(1.0)
-            x = np.array(embedding)
+            x = np.array(embedding, dtype=np.double)
 
             datapoints.append((x, y))
 
@@ -30,38 +31,56 @@ def read_dataset(filepath: str):
 def sig(x):
     return 1 / (1 + np.exp(-x))
 
+
 def sig_deriv(x):
     return sig(x)*(1-sig(x))
+
+
+def loss(y, y_hat):
+    return (y_hat - y)**2 
+
+
+def square_loss(datapoints, w):
+    sum = 0
+
+    for x, y in datapoints:
+        sum += (forward(x, w) - y) ** 2
+
+    return sum
+
+
+def accuracy(datapoints, w):
+    tp = 0
+    for x, y in datapoints:
+        predicted = np.round(forward(x, w))
+
+        if predicted == y:
+            tp += 1
+
+    return tp / len(datapoints)
 
 # ----------------------------------------------------------------------------------------------------------------------
 #               5.3 TRAINING
 # ----------------------------------------------------------------------------------------------------------------------
 
 def forward(x, w):
-    print(f"x.shape={x.shape}, w.shape={w.shape}")
     return sig(x.T.dot(w)) 
+
 
 def backward(w, lr, batch):
 
-    sum = np.zeros(len(batch))
+    sum = np.zeros(w.shape)
 
     for x, y in batch:
 
-        part1 = (sig(x.T.dot(w)) - y)
-        part2 = sig_deriv(x.T.dot(w))
+        # print(f"x.shape={x.shape}, y.shape={y.shape}, w.shape={w.shape}")
 
-        print(f"part1.shape={part1.shape}, part2.shape={part2.shape}, x.shape={x.shape}")
+        sum += (forward(x, w) - y) * sig_deriv(x.T.dot(w)) * x
 
-        sum += part1 * part2 * x
-
-    print(f"sum.shape={sum.shape}, w.shape={w.shape}")
+    # print(f"sum.shape={sum.shape}, w.shape={w.shape}")
 
     w -= (lr / len(batch)) * sum
 
-
-
-def loss(y, y_hat):
-    return (y_hat - y)**2 
 
 def train():
     # initialize weights randomly (with seed)
@@ -72,20 +91,21 @@ def train():
     datapoints_test = read_dataset("DATA/rt-polarity.test.vecs")
     datapoints_train = read_dataset("DATA/rt-polarity.train.vecs")
 
-    datapoints = datapoints_dev
+    datapoints = datapoints_train
 
     # organize input in batches
     # shuffle batches
 
-    N = len(datapoints[0][0]) # number of lines/datapoints
-    w = np.random.normal(0, 1, (N, 1)) 
+    N = len(datapoints[0][0]) # length of embedding vec
+
+    # reshaping is very important here to allow multiplication in backward()
+    w = np.random.normal(0, 1, (N, 1)).reshape(101,)
 
     lr = .01
-    n_epochs = 50
+    n_epochs = 300
     batch_size = 10
 
     n_batches = np.ceil(len(datapoints) / batch_size)
-    print(f"n_batches={n_batches}")
 
     for epoch in range(n_epochs):
 
@@ -103,14 +123,22 @@ def train():
             for x, y in batch:
                 # forward
                 y_hat = forward(x, w)
-                print(f"y_hat.shape={y_hat.shape}")
 
                 loss_sum += loss(y, y_hat)
 
-            print(f"epoch={epoch}, n_batch={n_batch}, loss_sum={loss_sum}")
+            #print(f"epoch={epoch}, n_batch={n_batch}, loss_sum={loss_sum}")
 
             # backward
             backward(w, lr, batch)
+
+        # print matrics
+        print("epoch={}/{}\n\tsquare_loss(dev)={:.2f}\n\tsquare_loss(test)={:.2f}\n\tacc(dev)={:.2f}\n\tacc(test)={:.2f}\n".format(
+            epoch, n_epochs,
+            square_loss(datapoints_dev, w), 
+            square_loss(datapoints_test, w), 
+            accuracy(datapoints_dev, w),
+            accuracy(datapoints_test, w)
+        ))
 
 if __name__ == "__main__":
     train()