bc8c91db844da5ee24b310f56e13a4797d5d3a9c
Elias Wendt <git@eliaswendt.de>
2022-05-10 12:27:19 +0200

    Make HW2 ready for submission

--- Homework_2/p1.py
+++ Homework_2/p1.py
@@ -4,15 +4,14 @@ from gensim.test.utils import datapath
 from gensim.models import KeyedVectors
 from scipy.stats import pearsonr
 
-# Data Reader
-
+# paths
 simlex = "SimLex-999/SimLex-999.txt"
 word2vec = "GoogleNews-vectors-negative300.bin"
 
 
 def read_simlex(filepath):
 
-    data = dict()
+    simlex = dict()
 
     with open(filepath) as f:
         next(f)  # skip the first line in file (csv header)
@@ -20,13 +19,9 @@ def read_simlex(filepath):
             columns = line.split("\t")
             columns[-1] = columns[-1][:-1]  # cut off newline char in last column
 
-            data[(columns[0], columns[1])] = float(columns[3])
+            simlex[(columns[0], columns[1])] = float(columns[3])
 
-    return data
-
-def sim_score(data, word1, word2):
-    return data[(word1, word2)]
-
+    return simlex
 
 # 1.3 Ranking Based on Word2Vec
 
@@ -49,28 +44,31 @@ def word2vec_distance(wv, word1, word2):
     return calculate_euclidean_distance(word1_vec, word2_vec)
 
 
-# 1.4 Correlation
+def task_1_2(simlex, word_pairs):
+    for word1, word2 in word_pairs:
+        print(f"SimLex-999 {word1} - {word2}: {simlex[(word1, word2)]}")
 
-def compute_correlation(data, wv, word_pairs):
+
+def task_1_3(wv, word_pairs):
+    for word1, word2 in word_pairs:
+        print(f"word2vec euclidean distance {word1} - {word2}: {word2vec_distance(wv, word1, word2)}")
+
+def task_1_4(simlex, wv, word_pairs):
     simlex_similarities = []
     euclidean_distances = []
 
     for word1, word2 in word_pairs:
-        #print(f"{word1} - {word2} -> {compute_correlation(data, wv, word1, word2)}")
-        simlex_similarities.append(sim_score(data, word1, word2))
+        simlex_similarities.append(simlex[(word1, word2)])
         euclidean_distances.append(word2vec_distance(wv, word1, word2))
 
-    return pearsonr(simlex_similarities, euclidean_distances)
+    print(f"pearson coefficient {word1} - {word2}: {pearsonr(simlex_similarities, euclidean_distances)[0]}") 
         
 
-
-
-
 if __name__ == "__main__":
-    data = read_simlex(simlex)
+    simlex = read_simlex(simlex)
     wv = KeyedVectors.load_word2vec_format(word2vec, binary=True, limit=50000)
     word_pairs = [('happy', 'cheerful'), ('happy', 'young'),('happy', 'angry')]
-    print(compute_correlation(data, wv, word_pairs)[0])
-    print('Pair "happy, cheerful": ', sim_score(data, "happy", "cheerful"))
-    print('Pair "happy, young": ', sim_score(data, "happy", "young"))
-    print('Pair "happy, angry": ', sim_score(data, "happy", "angry"))
+  
+    task_1_2(simlex, word_pairs)
+    task_1_3(wv, word_pairs)
+    task_1_4(simlex, wv, word_pairs)
